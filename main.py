9ed6773a8adb5b20299f7f260abe1f256536c6b4
Timotraque <157532049+Timotraque@users.noreply.github.com>
2024-02-08 15:30:23 -0500

    Ajout du choix de distribution

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ corde = int(input("Entrez la corde du profil en m : "))
 nb_points = int(input("Entrez le nombre de points le long de la corde : "))
 c = np.linspace(0, corde, nb_points)
 
-# Transformée de Glauert
-theta = np.linspace(0, np.pi, nb_points)
-x_c = 0.5 * (1 - np.cos(theta))
+choix = int(input("Distributions de points : \n1 - linéaire\n2 - non linéaire\nChoix : "))
+if choix == 1 :
+    x_c = np.linspace(0,corde,nb_points)
+else :
+    # Transformée de Glauert
+    theta = np.linspace(0, np.pi, nb_points)
+    x_c = 0.5 * (1 - np.cos(theta))
 
 # Equation de la courbe
 y_t = 5 * t * (0.2969 * np.sqrt(x_c) - 0.126 * x_c - 0.3516 * np.power(x_c, 2) + 0.2843 * np.power(x_c,3) - 0.1036 * np.power(x_c,4))
@@ -31,6 +35,7 @@ plt.xlabel('corde [m]')
 plt.ylabel('épaisseur [m]')
 plt.legend()
 plt.grid()
-plt.title('Affichage profil')
+plt.title('Profil NACA00' +str(int(t*100)))
+plt.xlim(-0.1,corde+0.1)
 plt.ylim(-0.3*corde,0.3*corde)      # Contraint l'affichage pour garder les proportions
 plt.show()
